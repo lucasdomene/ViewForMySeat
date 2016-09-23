0ef8998841e4bb601500c87332c15ab4b937ee62
Lucas Firmo <lucas.firmo@gmail.com>
2016-09-23 14:53:32 -0300

    Creating FeaturedPhotos Data Source

---
+++ ViewFromMySeat/ViewFromMySeat/FeaturedPhotosDataSource.h
@@ -0,0 +1,18 @@
+//
+//  FeaturedPhotosDataSource.h
+//  ViewFromMySeat
+//
+//  Created by Lucas Domene Firmo on 9/23/16.
+//  Copyright © 2016 Domene. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface FeaturedPhotosDataSource : NSObject <UITableViewDataSource>
+
+@property (nonatomic, copy) NSArray *featuredPhotos;
+
+- (instancetype)initWithFeaturedPhotos:(NSArray *)featuredPhotos;
+
+@end
---
+++ ViewFromMySeat/ViewFromMySeat/FeaturedPhotosDataSource.m
@@ -0,0 +1,42 @@
+//
+//  FeaturedPhotosDataSource.m
+//  ViewFromMySeat
+//
+//  Created by Lucas Domene Firmo on 9/23/16.
+//  Copyright © 2016 Domene. All rights reserved.
+//
+
+#import "FeaturedPhotosDataSource.h"
+#import "FeaturedPhotoTableViewCell.h"
+#import "FeaturedPhoto.h"
+
+@implementation FeaturedPhotosDataSource
+
+- (instancetype)initWithFeaturedPhotos:(NSArray *)featuredPhotos {
+    self = [super init];
+    if (self) {
+        _featuredPhotos = featuredPhotos;
+    }
+    return self;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return _featuredPhotos.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    FeaturedPhotoTableViewCell * cell = [tableView dequeueReusableCellWithIdentifier:@"FeaturedPhotoCell" forIndexPath:indexPath];
+    FeaturedPhoto * featuredPhoto = _featuredPhotos[indexPath.row];
+    
+    cell.venueNameLabel.text = featuredPhoto.venue;
+    
+    NSString * seatInfo = [NSString stringWithFormat:@"Section: %@, row: %@, seat: %@", featuredPhoto.section, featuredPhoto.row, featuredPhoto.seat];
+    
+    cell.seatLabel.text = seatInfo;
+    cell.numberOfViewsLabel.text = featuredPhoto.views;
+    cell.notesLabel.text = featuredPhoto.note;
+    
+    return cell;
+}
+
+@end
