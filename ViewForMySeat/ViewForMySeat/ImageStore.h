fd920283e47c3e61e85bda0843d3c6a98c72b73e
Lucas Firmo <lucas.firmo@gmail.com>
2016-09-23 00:39:09 -0300

    Creating ImageStore to cache images

---
+++ ViewForMySeat/ViewForMySeat/ImageStore.h
@@ -0,0 +1,18 @@
+//
+//  ImageStore.h
+//  ViewForMySeat
+//
+//  Created by Lucas Domene Firmo on 9/23/16.
+//  Copyright © 2016 Domene. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface ImageStore : NSObject
+
++ (ImageStore *)sharedInstance;
+- (void)cacheImage:(UIImage *)image forKey:(NSString *)key;
+- (UIImage *)imageForKey:(NSString *)key;
+
+@end
---
+++ ViewForMySeat/ViewForMySeat/ImageStore.m
@@ -0,0 +1,46 @@
+//
+//  ImageStore.m
+//  ViewForMySeat
+//
+//  Created by Lucas Domene Firmo on 9/23/16.
+//  Copyright © 2016 Domene. All rights reserved.
+//
+
+#import "ImageStore.h"
+
+static ImageStore *sharedInstance;
+
+@interface ImageStore()
+
+@property (nonatomic, strong) NSCache *imageCache;
+
+@end
+
+@implementation ImageStore
+
++ (ImageStore *)sharedInstance {
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedInstance = [[ImageStore alloc] init];
+    });
+    return sharedInstance;
+}
+
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        self.imageCache = [[NSCache alloc] init];
+    }
+    return self;
+}
+
+
+- (void)cacheImage:(UIImage *)image forKey:(NSString *)key {
+    [self.imageCache setObject:image forKey:key];
+}
+
+- (UIImage *)imageForKey:(NSString *)key {
+    return [self.imageCache objectForKey:key];
+}
+
+@end
