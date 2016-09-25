cf0d436ac933811145f1d3ab1447e37c6ab2b7f2
Lucas Firmo <lucas.firmo@gmail.com>
2016-09-25 14:01:18 -0300

    Creating VenueLocationCell

---
+++ ViewFromMySeat/ViewFromMySeat/VenueLocationTableViewCell.h
@@ -0,0 +1,17 @@
+//
+//  VenueLocationTableViewCell.h
+//  ViewFromMySeat
+//
+//  Created by Lucas Domene Firmo on 9/25/16.
+//  Copyright © 2016 Domene. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VenueLocationTableViewCell : UITableViewCell
+
+@property (strong, nonatomic) IBOutlet UILabel *addressLabel;
+@property (strong, nonatomic) IBOutlet UILabel *cityAndStateLabel;
+@property (strong, nonatomic) IBOutlet UILabel *countryLabel;
+
+@end
---
+++ ViewFromMySeat/ViewFromMySeat/VenueLocationTableViewCell.m
@@ -0,0 +1,17 @@
+//
+//  VenueLocationTableViewCell.m
+//  ViewFromMySeat
+//
+//  Created by Lucas Domene Firmo on 9/25/16.
+//  Copyright © 2016 Domene. All rights reserved.
+//
+
+#import "VenueLocationTableViewCell.h"
+
+@implementation VenueLocationTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+}
+
+@end
